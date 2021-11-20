56fd46237675df8d881a6e69132cc7002d5466a4
Silverhaze <daansmits_@hotmail.com>
2021-11-20 01:04:18 +0100

    First Commit

---
+++ BeatSaberMapFilter/MainWindow.xaml.cs
@@ -0,0 +1,253 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeatSaberMapFilter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IEnumerable<Models.DiffElement> _shownMapDataResult;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //// x y
+            //var dic = new Dictionary<double, double>();
+            //dic.Add(1, 40);
+            //dic.Add(2, 50);
+            //dic.Add(3, 70);
+            //dic.Add(4, 9);
+            //dic.Add(5, 12);
+
+            //DrawGraph(dic);
+        }
+
+        private void DrawGraph(Dictionary<double, double> points)
+        {
+            var width = Canvas.Width;
+            var height = Canvas.Height;
+
+            var spaceWidth = width / (points.Keys.Max()/* - points.Keys.Min()*/);
+            var spaceHeight = height / (points.Values.Max()/* - points.Values.Min()*/);
+
+            //Max Value Line and text
+            var topLine = new Line();
+            topLine.X1 = 0;
+            topLine.X2 = width;
+            topLine.Y1 = height - points.Values.Max() * spaceHeight;
+            topLine.Y2 = height - points.Values.Max() * spaceHeight;
+            topLine.Stroke = System.Windows.Media.Brushes.Black;
+            topLine.HorizontalAlignment = HorizontalAlignment.Left;
+            topLine.VerticalAlignment = VerticalAlignment.Center;
+            var topLineText = new TextBlock();
+            topLineText.Text = Math.Round(points.Values.Max(), 2).ToString();
+            topLineText.Foreground = new SolidColorBrush(Color.FromRgb(237, 242, 244));
+            Canvas.SetLeft(topLineText, topLine.X1 + width + 10);
+            Canvas.SetTop(topLineText, topLine.Y1);
+            Canvas.Children.Add(topLine);
+            Canvas.Children.Add(topLineText);
+
+            //Avg Value Line and text
+            var avgLine = new Line();
+            avgLine.X1 = 0;
+            avgLine.X2 = width;
+            avgLine.Y1 = height - points.Values.Average() * spaceHeight;
+            avgLine.Y2 = height - points.Values.Average() * spaceHeight;
+            avgLine.Stroke = System.Windows.Media.Brushes.Black;
+            avgLine.HorizontalAlignment = HorizontalAlignment.Left;
+            avgLine.VerticalAlignment = VerticalAlignment.Center;
+            var avgLineText = new TextBlock();
+            avgLineText.Text = Math.Round(points.Values.Average(), 2).ToString();
+            avgLineText.Foreground = new SolidColorBrush(Color.FromRgb(237, 242, 244));
+            Canvas.SetLeft(avgLineText, avgLine.X1 + width + 10);
+            Canvas.SetTop(avgLineText, avgLine.Y1);
+            Canvas.Children.Add(avgLine);
+            Canvas.Children.Add(avgLineText);
+
+            //Min Value Line and text
+            var minLine = new Line();
+            minLine.X1 = 0;
+            minLine.X2 = width;
+            minLine.Y1 = height - points.Values.Min() * spaceHeight;
+            minLine.Y2 = height - points.Values.Min() * spaceHeight;
+            minLine.Stroke = System.Windows.Media.Brushes.Black;
+            minLine.HorizontalAlignment = HorizontalAlignment.Left;
+            minLine.VerticalAlignment = VerticalAlignment.Center;
+            var minLineText = new TextBlock();
+            minLineText.Text = Math.Round(points.Values.Min(), 2).ToString();
+            minLineText.Foreground = new SolidColorBrush(Color.FromRgb(237, 242, 244));
+            Canvas.SetLeft(minLineText, minLine.X1 + width + 10);
+            Canvas.SetTop(minLineText, minLine.Y1);
+            Canvas.Children.Add(minLine);
+            Canvas.Children.Add(minLineText);
+
+            //Draw All datapoint lines
+            for (var x = 0; x < points.Count; x++)
+            {
+                if (x + 1 == points.Count) continue;
+                var line = new Line();
+                line.Stroke = System.Windows.Media.Brushes.Red;
+                line.HorizontalAlignment = HorizontalAlignment.Left;
+                line.VerticalAlignment = VerticalAlignment.Center;
+                line.StrokeThickness = 20 / Math.Sqrt(points.Count);
+                line.X1 = (points.Keys.ElementAt(x) * spaceWidth) - spaceWidth;
+                line.X2 = (points.Keys.ElementAt(x + 1) * spaceWidth) - spaceWidth;
+                line.Y1 = height - (points.Values.ElementAt(x) * spaceHeight);
+                line.Y2 = height - (points.Values.ElementAt(x + 1) * spaceHeight);
+                //line.Y1 = (height - ((points.Values.ElementAt(x) * spaceHeight) - spaceHeight)) + (points.Values.ElementAt(0) * spaceHeight);
+                //line.Y2 = (height - ((points.Values.ElementAt(x + 1) * spaceHeight) - spaceHeight)) + (points.Values.ElementAt(0) * spaceHeight);
+                Canvas.Children.Add(line);
+            }
+        }
+
+        private async void ShowResultsButton_Click(object sender, RoutedEventArgs e)
+        {
+            graphCombobox.Items.Clear();
+
+            var downloadMin = Convert.ToInt32(DownloadMinInput.Text);
+            var downloadMax = Convert.ToInt32(DownloadMaxInput.Text);
+            var upvotesMin = Convert.ToInt32(UpvotesMinInput.Text);
+            var upvotesMax = Convert.ToInt32(UpvotesMaxInput.Text);
+            var ratioMin = Convert.ToInt32(RatioMinInput.Text);
+            var ratioMax = Convert.ToInt32(RatioMaxInput.Text);
+            var downvotesMin = Convert.ToInt32(DownloadMinInput.Text);
+            var downvotesMax = Convert.ToInt32(DownloadMaxInput.Text);
+            var bpmMin = Convert.ToInt32(BPMMinInput.Text);
+            var bpmMax = Convert.ToInt32(BPMMaxInput.Text);
+            var njsMin = Convert.ToDouble(NJSMinInput.Text);
+            var njsMax = Convert.ToDouble(NJSMaxInput.Text);
+            var npsMin = Convert.ToInt32(NPSMinInput.Text);
+            var npsMax = Convert.ToInt32(NPSMaxInput.Text);
+            var ntsMin = Convert.ToInt32(NotesMinInput.Text);
+            var ntsMax = Convert.ToInt32(NotesMaxInput.Text);
+            var starsMin = Convert.ToDouble(StarsMinInput.Text);
+            var starsMax = Convert.ToDouble(StarsMaxInput.Text);
+            var ppMin = Convert.ToDouble(PPMinInput.Text);
+            var ppMax = Convert.ToDouble(PPMaxInput.Text);
+
+            using (var httpClient = new HttpClient())
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("https://cdn.wes.cloud/beatstar/bssb/v2-all.json");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                //Collection with hashcode as key and mapMainDetails as value
+                var allBeatSaberMaps = JsonConvert.DeserializeObject<Dictionary<string, Models.Map>>(responseBody);
+
+                //Collection with only mapMainDetails 
+                var allBeatSaberMapsValues = allBeatSaberMaps.Values;
+
+                //Collection with only mapSpecificDetails (has duplicate keyCodes)
+                var AllMaps = new List<Models.DiffElement>();
+                foreach (var diff in allBeatSaberMapsValues)
+                {
+                    foreach (var map in diff.Diffs)
+                    {
+                        //Add More map details. Also calculate more map details [TODO]
+                        map.KeyCode = diff.Key;
+                        if (map.Length > 0 && map.Notes > 0) map.Nps = map.Notes / map.Length;
+                        map.Pp = map.Pp.Replace(".", ",");
+                        map.Star = map.Star.Replace(".", ",");
+                        if (Convert.ToDouble(map.Pp) > 0) map.IsRanked = true;
+                        map.BPM = diff.Bpm;
+                        map.DownloadCount = diff.DownloadCount;
+                        map.Upvotes = diff.UpVotes;
+                        map.Downvotes = diff.DownVotes;
+                        map.Heat = diff.Heat;
+                        map.Rating = diff.Rating;
+                        map.UploadDate = diff.Uploaddate;
+
+                        //Check for every filter option if the map should be added or not
+                        var shouldBeAdded = true;
+                        //Global Filters--------------------
+                        if ((bool)GlobalEnabledCheckBox.IsChecked)
+                        {
+                            if (!(diff.Bpm >= bpmMin && diff.Bpm <= bpmMax)) shouldBeAdded = false;
+                            if (!(diff.DownloadCount >= downloadMin && diff.DownloadCount <= downloadMax)) shouldBeAdded = false;
+                            if (!(diff.UpVotes >= upvotesMin && diff.UpVotes <= upvotesMax)) shouldBeAdded = false;
+                            if (!(diff.DownVotes >= downvotesMin && diff.DownVotes <= downvotesMax)) shouldBeAdded = false;
+                            if (!((diff.Rating * 100) >= ratioMin && (diff.Rating * 100) <= ratioMax)) shouldBeAdded = false;
+                            if (EasyCheckBox.IsChecked == false && map.Diff == "Easy") shouldBeAdded = false;
+                            if (NormalCheckBox.IsChecked == false && map.Diff == "Normal") shouldBeAdded = false;
+                            if (HardCheckBox.IsChecked == false && map.Diff == "Hard") shouldBeAdded = false;
+                            if (ExpertCheckBox.IsChecked == false && map.Diff == "Expert") shouldBeAdded = false;
+                            if (ExpertPlusCheckBox.IsChecked == false && map.Diff == "Expert+") shouldBeAdded = false;
+                        }
+
+                        //Map Filters--------------------                      
+                        if ((bool)MapEnabledCheckBox.IsChecked)
+                        {
+                            if (!(map.Njs >= njsMin && map.Njs <= njsMax)) shouldBeAdded = false;
+                            if (!(map.Nps >= npsMin && map.Nps <= npsMax)) shouldBeAdded = false;
+                            if (!(map.Notes >= ntsMin && map.Notes <= ntsMax)) shouldBeAdded = false;
+                        }
+
+                        //ScoreSaber Filters-----------------
+                        if ((bool)ScoresaberEnabledCheckBox.IsChecked)
+                        {
+                            if (!(Convert.ToDouble(map.Star) >= starsMin && Convert.ToDouble(map.Star) <= starsMax)) shouldBeAdded = false;
+                            if (!(Convert.ToDouble(map.Pp) >= ppMin && Convert.ToDouble(map.Pp) <= ppMax)) shouldBeAdded = false;
+                            if ((bool)RankedOnlyCheckBox.IsChecked) if (!map.IsRanked) shouldBeAdded = false;
+                        }
+
+                        if (shouldBeAdded) AllMaps.Add(map);
+                    }
+                };
+
+                //Max range to output. 
+                var maxOutputCount = Convert.ToInt32(MaxOutputInput.Text);
+                if (maxOutputCount == 0) maxOutputCount = AllMaps.Count();
+                var rangedMapDifficulties = AllMaps.Take(maxOutputCount);
+                ResultsCountLabel.Content = $"Total Results: {AllMaps.Count()} - Shown Results: {maxOutputCount}";
+                MapDataGrid.ItemsSource = rangedMapDifficulties;
+
+
+
+
+
+                //Add colum headers into combobox from graph
+                foreach (var colum in MapDataGrid.Columns)
+                {
+                    if (colum.Header.ToString() == "Scores" || colum.Header.ToString() == "UploadDate" || colum.Header.ToString() == "KeyCode" || colum.Header.ToString() == "Diff") continue;
+                    graphCombobox.Items.Add(colum.Header);
+                }
+                _shownMapDataResult = rangedMapDifficulties;
+            }
+        }
+
+        private void graphCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Graph clear
+            Canvas.Children.Clear();
+
+            //Draw Graph
+            var dic = new Dictionary<double, double>();
+
+            if (graphCombobox.SelectedItem == null) return;
+            var prop = typeof(Models.DiffElement).GetProperty(graphCombobox.SelectedItem.ToString());
+            var count = 1;
+            foreach (var mapData in _shownMapDataResult)
+            {
+                var value = prop.GetValue(mapData);
+                dic.Add(count, Convert.ToDouble(value));
+                count++;
+            }
+            DrawGraph(dic);
+        }
+    }
+}
---
+++ BeatSaberMapFilter/Models/MapsModel.cs
@@ -0,0 +1,333 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Globalization;
+
+
+namespace BeatSaberMapFilter.Models
+{
+    public partial class Map
+    {
+        [JsonProperty("diffs")]
+        public DiffElement[] Diffs { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("mapper")]
+        public string Mapper { get; set; }
+
+        [JsonProperty("song")]
+        public string Song { get; set; }
+
+        [JsonProperty("bpm")]
+        public long Bpm { get; set; }
+
+        [JsonProperty("downloadCount")]
+        public long DownloadCount { get; set; }
+
+        [JsonProperty("upVotes")]
+        public long UpVotes { get; set; }
+
+        [JsonProperty("downVotes")]
+        public long DownVotes { get; set; }
+
+        [JsonProperty("heat")]
+        public double Heat { get; set; }
+
+        [JsonProperty("rating")]
+        public double Rating { get; set; }
+
+        //[JsonProperty("automapper")]
+        //public AutomapperUnion Automapper { get; set; }
+
+        [JsonProperty("uploaddate")]
+        public DateTimeOffset? Uploaddate { get; set; }
+    }
+
+    public partial class DiffElement
+    {
+        public string KeyCode { get; set; }
+
+        [JsonProperty("diff")]
+        public string Diff { get; set; }
+
+
+        [JsonProperty("njs")]
+        public long Njs { get; set; }
+
+        public long Nps { get; set; }
+
+        public long BPM { get; set; }
+
+        [JsonProperty("scores")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long Scores { get; set; }
+
+
+        [JsonProperty("type")]
+        public long Type { get; set; }
+
+        [JsonProperty("len")]
+        public long Length { get; set; }
+
+        [JsonProperty("njt")]
+        public long Njt { get; set; }
+
+        [JsonProperty("bmb")]
+        public long Bombs { get; set; }
+
+        [JsonProperty("nts")]
+        public long Notes { get; set; }
+
+        [JsonProperty("obs")]
+        public long Obstacles { get; set; }
+
+        [JsonProperty("pp")]
+        public string Pp { get; set; }
+
+        [JsonProperty("star")]
+        public string Star { get; set; }
+
+        public long DownloadCount { get; set; }
+        public long Upvotes { get; set; }
+        public long Downvotes { get; set; }
+        public double Rating { get; set; }
+        public double Heat { get; set; }
+        public DateTimeOffset? UploadDate { get; set; }
+
+        public bool IsRanked = false;
+    }
+
+    public enum AutomapperEnum { Beatsage, Deepsaber, Lolighter };
+
+    public enum DiffEnum { Easy, Normal, Hard, Expert, ExpertPlus };
+
+    public partial struct AutomapperUnion
+    {
+        public AutomapperEnum? Enum;
+        public long? Integer;
+
+        public static implicit operator AutomapperUnion(AutomapperEnum Enum) => new AutomapperUnion { Enum = Enum };
+        public static implicit operator AutomapperUnion(long Integer) => new AutomapperUnion { Integer = Integer };
+        public bool IsNull => Integer == null && Enum == null;
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters =
+            {
+                AutomapperUnionConverter.Singleton,
+                AutomapperEnumConverter.Singleton,
+                DiffEnumConverter.Singleton,
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class AutomapperUnionConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(AutomapperUnion) || t == typeof(AutomapperUnion?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.Null:
+                    return new AutomapperUnion { };
+                case JsonToken.String:
+                case JsonToken.Date:
+                    var stringValue = serializer.Deserialize<string>(reader);
+                    switch (stringValue)
+                    {
+                        case "beatsage":
+                            return new AutomapperUnion { Enum = AutomapperEnum.Beatsage };
+                        case "deepsaber":
+                            return new AutomapperUnion { Enum = AutomapperEnum.Deepsaber };
+                        case "lolighter":
+                            return new AutomapperUnion { Enum = AutomapperEnum.Lolighter };
+                    }
+                    long l;
+                    if (Int64.TryParse(stringValue, out l))
+                    {
+                        return new AutomapperUnion { Integer = l };
+                    }
+                    break;
+            }
+            throw new Exception("Cannot unmarshal type AutomapperUnion");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            var value = (AutomapperUnion)untypedValue;
+            if (value.IsNull)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            if (value.Enum != null)
+            {
+                switch (value.Enum)
+                {
+                    case AutomapperEnum.Beatsage:
+                        serializer.Serialize(writer, "beatsage");
+                        return;
+                    case AutomapperEnum.Deepsaber:
+                        serializer.Serialize(writer, "deepsaber");
+                        return;
+                    case AutomapperEnum.Lolighter:
+                        serializer.Serialize(writer, "lolighter");
+                        return;
+                }
+            }
+            if (value.Integer != null)
+            {
+                serializer.Serialize(writer, value.Integer.Value.ToString());
+                return;
+            }
+            throw new Exception("Cannot marshal type AutomapperUnion");
+        }
+
+        public static readonly AutomapperUnionConverter Singleton = new AutomapperUnionConverter();
+    }
+
+    internal class AutomapperEnumConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(AutomapperEnum) || t == typeof(AutomapperEnum?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "beatsage":
+                    return AutomapperEnum.Beatsage;
+                case "deepsaber":
+                    return AutomapperEnum.Deepsaber;
+                case "lolighter":
+                    return AutomapperEnum.Lolighter;
+            }
+            throw new Exception("Cannot unmarshal type AutomapperEnum");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (AutomapperEnum)untypedValue;
+            switch (value)
+            {
+                case AutomapperEnum.Beatsage:
+                    serializer.Serialize(writer, "beatsage");
+                    return;
+                case AutomapperEnum.Deepsaber:
+                    serializer.Serialize(writer, "deepsaber");
+                    return;
+                case AutomapperEnum.Lolighter:
+                    serializer.Serialize(writer, "lolighter");
+                    return;
+            }
+            throw new Exception("Cannot marshal type AutomapperEnum");
+        }
+
+        public static readonly AutomapperEnumConverter Singleton = new AutomapperEnumConverter();
+    }
+
+    internal class DiffEnumConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(DiffEnum) || t == typeof(DiffEnum?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            switch (value)
+            {
+                case "Easy":
+                    return DiffEnum.Easy;
+                case "Expert":
+                    return DiffEnum.Expert;
+                case "Expert+":
+                    return DiffEnum.ExpertPlus;
+                case "Hard":
+                    return DiffEnum.Hard;
+                case "Normal":
+                    return DiffEnum.Normal;
+            }
+            throw new Exception("Cannot unmarshal type DiffEnum");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (DiffEnum)untypedValue;
+            switch (value)
+            {
+                case DiffEnum.Easy:
+                    serializer.Serialize(writer, "Easy");
+                    return;
+                case DiffEnum.Expert:
+                    serializer.Serialize(writer, "Expert");
+                    return;
+                case DiffEnum.ExpertPlus:
+                    serializer.Serialize(writer, "Expert+");
+                    return;
+                case DiffEnum.Hard:
+                    serializer.Serialize(writer, "Hard");
+                    return;
+                case DiffEnum.Normal:
+                    serializer.Serialize(writer, "Normal");
+                    return;
+            }
+            throw new Exception("Cannot marshal type DiffEnum");
+        }
+
+        public static readonly DiffEnumConverter Singleton = new DiffEnumConverter();
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+}
+
+
+
